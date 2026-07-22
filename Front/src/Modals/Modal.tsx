fef4ccf5535093a9410522c3df5b77dc9605e28e
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-07-22 15:50:27 -0700

    style: Implementación de una animación moderna al modal de rangos

--- Front/src/Modals/Modal.tsx
+++ Front/src/Modals/Modal.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React, { useState } from "react";
 import type { ReactNode, SetStateAction } from "react";
 import { createPortal } from "react-dom";
 import { X } from "@boxicons/react";
@@ -10,10 +10,26 @@ interface ModalProps {
 }
 
 export default function Modal({ children, setOpenModal }: ModalProps) {
+    const [isClose, setIsClose] = useState(false);
+
+    const closeModal = () => {
+        setIsClose(true);
+    };
+
+    const handleAnimationEnd = () => {
+        if (isClose) {
+            setOpenModal(false);
+        }
+    };
+
     return createPortal(
-        <div className="overlay" onClick={() => setOpenModal(false)}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <button className="button-close-modal" onClick={() => setOpenModal(false)}><X/></button>
+        <div
+            className={`overlay ${isClose ? "closing" : "opening"}`}
+            onClick={closeModal}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            <div className={`modal ${isClose ? "closing" : "opening"}`} onClick={(e) => e.stopPropagation()}>
+                <button className="button-close-modal" onClick={closeModal}><X /></button>
                 {children}
             </div>
         </div>,
