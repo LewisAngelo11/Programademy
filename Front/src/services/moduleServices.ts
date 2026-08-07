4709aa2a8e6e6ab8f7749c7a87ac7e34276fe9d6
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-08-07 12:13:29 -0700

    refactor: Eliminar código ya no necesario en moduleServices

--- Front/src/services/moduleServices.ts
+++ Front/src/services/moduleServices.ts
@@ -1,7 +1,5 @@
 import { apiFetch } from "./apiFetch";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export class ModuloService {
     static async getAllModulesFromCourse(idCurso: number) {
         const response = await apiFetch(`/modulo/course/all/${idCurso}`, {
@@ -66,52 +64,4 @@ export class ModuloService {
 
         return await response.json();
     }
-}
-
-export const getAllModulesFromCourse = async (token: string, idCurso: number) => {
-    const response = await fetch(`${API_URL}/modulo/course/all/${idCurso}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    });
-
-    if (response.status === 401) {
-        return {
-            error: "No autorizado.",
-            status: 401
-        };
-    }
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.error || 'Error al obtener los módulos');
-    }
-
-    return await response.json();
-}
-
-export const getOneModule = async (token: string, idModulo: number) => {
-    const response = await fetch(`${API_URL}/modulo/get/${idModulo}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    });
-
-    if (response.status === 401) {
-        return {
-            error: "No autorizado.",
-            status: 401
-        };
-    }
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.error || 'Error al consultar el módulo');
-    }
-
-    return await response.json();
 }
